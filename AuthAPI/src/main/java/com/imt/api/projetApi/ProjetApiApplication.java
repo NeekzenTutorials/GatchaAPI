ce9310816d214fa1f19f0d0e96dd0536813c2027
Nathan Fourny <nathan.fourny@preditwins.com>
2026-01-14 14:00:37 +0100

    init API project

---
+++ AuthAPI/src/main/java/com/imt/api/projetApi/ProjetApiApplication.java
@@ -0,0 +1,13 @@
+package com.imt.api.projetApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetApiApplication.class, args);
+	}
+
+}
